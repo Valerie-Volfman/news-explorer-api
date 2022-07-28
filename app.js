3705bfebaa711a8eb7964124114e74bd2f76bd2f
Valerie Volfman <valerie@MBP-Valerie.lan>
2022-07-28 20:49:53 +0300

    feat: index.js routes file

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const limiter = require('./middlewares/limiter');
-const userRouter = require('./routes/users');
-const articleRouter = require('./routes/articles');
-const { createUser, login } = require('./controllers/user');
+const router = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const auth = require('./middlewares/auth');
 const ErrorHandler = require('./middlewares/error-handler');
-const CentralError = require('./errors/central-error');
+
 
 const { PORT = 3000 } = process.env;
 
@@ -24,17 +21,7 @@ app.use(requestLogger);
 
 app.use(limiter);
 
-app.post('/signin', login);
-
-app.post('/signup', createUser);
-
-app.use(auth);
-
-app.use(userRouter);
-app.use(articleRouter);
-app.get('*', () => {
-  throw new CentralError(404, 'Requested resource not found');
-});
+app.use('/', router);
 
 app.use(errorLogger);
 app.use((err, req, res, next) => {
